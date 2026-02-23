c7974308a044aa3c90a32856a543eeb39d28d9a8
notjung <chanintales555@gmail.com>
2026-02-23 19:49:13 +0700

    Force clean private key string

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -8,7 +8,9 @@ export async function POST(req: Request) {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        private_key: process.env.GOOGLE_PRIVATE_KEY 
+  ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/"/g, '').trim()
+  : undefined,
       },
       scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
